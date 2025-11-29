4ed22be419d08e36bddd3001fbab1fd78214f422
Ingo Wolf <me@ingowolf.au>
2025-11-29 15:14:42 +1100

    reset project

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,7 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
 import { Stack } from "expo-router";
-import { StatusBar } from "expo-status-bar";
-import "react-native-reanimated";
 import { ConvexReactClient } from "convex/react";
 import { ConvexBetterAuthProvider } from "@convex-dev/better-auth/react";
 import { authClient } from "@/lib/auth-client";
-
-import { useColorScheme } from "@/hooks/use-color-scheme";
-
-export const unstable_settings = {
-  anchor: "(tabs)",
-};
 
 const convex = new ConvexReactClient(
   process.env.EXPO_PUBLIC_CONVEX_URL as string,
@@ -26,20 +13,9 @@ const convex = new ConvexReactClient(
 );
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <ConvexBetterAuthProvider client={convex} authClient={authClient}>
-      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen
-            name="modal"
-            options={{ presentation: "modal", title: "Modal" }}
-          />
-        </Stack>
-        <StatusBar style="auto" />
-      </ThemeProvider>{" "}
+      <Stack />
     </ConvexBetterAuthProvider>
   );
 }
