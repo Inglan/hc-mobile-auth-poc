1e3ed02016cbb25b1d20bab67eaf58d9e64ed1e5
Ingo Wolf <me@ingowolf.au>
2025-11-29 15:57:51 +1100

    use safegetauthuser

--- convex/auth.ts
+++ convex/auth.ts
@@ -79,6 +79,6 @@ export const createAuth = (
 export const getCurrentUser = query({
   args: {},
   handler: async (ctx) => {
-    return authComponent.getAuthUser(ctx);
+    return authComponent.safeGetAuthUser(ctx);
   },
 });
